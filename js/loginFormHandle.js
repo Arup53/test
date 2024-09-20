642a8b916cf98b2798ec1542b4c79af2f87bfb61
Arup 53 <arupkumarhowlader12@gmail.com>
2024-09-20 08:47:18 +0600

    2nd commit

--- js/loginFormHandle.js
+++ js/loginFormHandle.js
@@ -2,7 +2,7 @@ document.getElementById("login-button").addEventListener("click", function (e) {
   e.preventDefault();
   const pin = document.getElementById("password").value;
   if (pin === "5") {
-    window.location.href = "../home.html";
+    window.location.href = "/home.html";
   } else {
     alert("wrong password");
   }
